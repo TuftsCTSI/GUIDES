f9a5ba6f3d33edaef00be47c7ba6638c1b28ea51
kzollove <kzollovenecek@tuftsmedicalcenter.org>
2024-05-02 15:54:53 -0400

    Explore child handling

--- codeset.jl
+++ codeset.jl
@@ -47,8 +47,52 @@ Notebook implementation details.
 # ╔═╡ b00d388f-cf21-49c6-a985-2e011b8913b3
 @connect "ctsi.codeset" "ctsi.trdw_green" # shifted dates/times but no other PHI
 
+# ╔═╡ 1c70715b-85e0-483a-98cb-0f11c0bc06a9
+@query from(codeset_expression).filter(codeset_id == 106000255).limit(100)
+
+# ╔═╡ a0c28148-e8f8-4ef1-b7a5-aea58e41bc0f
+xf = @query concept(
+	concept_id == 4125593
+).limit(10)
+
+# ╔═╡ de70824c-deee-4b88-9f09-7be7a95a2320
+"""$(xf.ref.x[!, "vocabulary_id"][1])("$(xf.ref.x[!, "concept_code"][1])", $(xf.ref.x[!, "concept_name"][1]))"""
+
+# ╔═╡ 962c15d8-16d6-41b5-a6c7-f54bfdfcb483
+dd = @query concept_sets(
+	p = SNOMED("289203002", "Finding of pattern of pregnancy")
+)
+
+# ╔═╡ d6840fd8-879c-47bb-853c-322e26d5db9a
+dump(dd)
+
+# ╔═╡ 8a8c87b0-6ea3-4def-b115-a149da63386f
+ddd = @query concept(
+			concept_id == 4125593
+		).concept_children()
+
+# ╔═╡ 239c8f1f-2709-4db0-8d6d-36bbe8a98645
+for r in eachrow(ddd.ref.x)
+	print(r["concept_id"])
+end
+
+# ╔═╡ 4faa997b-2ae1-4440-887e-66d87b8d2e26
+dump(ddd)
+
+# ╔═╡ 024b5bd5-1e7a-48a1-83a6-081d47ff0c7c
+@query from(codeset_expression).filter(is_n3c_recommended == 1).limit(100)
+
 # ╔═╡ 9a9f06ba-07b9-4d73-b7ff-e680f5ced07b
-df = @query from(codeset).filter(is_n3c_recommended == 1)
+df = @query begin
+	from(codeset)
+	filter(is_n3c_recommended == 1)
+	left_join(r => from(researcher), r.researcher_id == codeset_created_by)
+	define(
+		r.researcher_name,
+		r.institution,
+		r.orcid_id
+	)
+end
 
 # ╔═╡ f980f781-c1f8-4e9c-bd22-cac6fb1dbd16
 begin
@@ -61,12 +105,16 @@ begin
 				<b>$(d["concept_set_name"]):</b> 
 				<p><b>$(d["number_concepts"])</b> concepts</p>
 			</summary>
-			<h3>$(d["concept_set_name"])</h3>
-			<h4>Intention</h4>
-			<p>$(d["codeset_intention"])</p>
-			<h4>Limitations</h4>
-			<p>$(d["limitations"])</p>
-			<h4>Number concepts: $(d["number_concepts"])</h4>
+			<div style="border:solid #E5E5E5;margin-bottom:10px;padding:20px;background-color:#FEFCF5;">
+				<h3>$(d["concept_set_name"])</h3>
+				<h4>Codeset author: </h4>
+				<p>$(d["researcher_name"]) ($(d["orcid_id"]), $(d["institution"]))</p>
+				<h4>Intention</h4>
+				<p>$(d["codeset_intention"])</p>
+				<h4>Limitations</h4>
+				<p>$(d["limitations"])</p>
+				<h4>Number concepts: $(d["number_concepts"])</h4>
+			</div>
 		</details>
 		</div>
 		"""))
@@ -76,7 +124,16 @@ end
 
 # ╔═╡ Cell order:
 # ╠═95a93876-78af-40a1-b55f-24062f7eddb0
-# ╟─f980f781-c1f8-4e9c-bd22-cac6fb1dbd16
+# ╠═f980f781-c1f8-4e9c-bd22-cac6fb1dbd16
+# ╠═1c70715b-85e0-483a-98cb-0f11c0bc06a9
+# ╠═a0c28148-e8f8-4ef1-b7a5-aea58e41bc0f
+# ╠═239c8f1f-2709-4db0-8d6d-36bbe8a98645
+# ╠═962c15d8-16d6-41b5-a6c7-f54bfdfcb483
+# ╠═4faa997b-2ae1-4440-887e-66d87b8d2e26
+# ╠═d6840fd8-879c-47bb-853c-322e26d5db9a
+# ╠═de70824c-deee-4b88-9f09-7be7a95a2320
+# ╠═8a8c87b0-6ea3-4def-b115-a149da63386f
+# ╠═024b5bd5-1e7a-48a1-83a6-081d47ff0c7c
 # ╟─d858cfa7-f0a0-4616-86da-9cebb90c6d65
 # ╠═9a9f06ba-07b9-4d73-b7ff-e680f5ced07b
 # ╠═c6f49bb2-783a-11ee-0151-47703d60127f
