7fe3d5d01562c8fc44101bb177c939e3422d25f2
kzollove <kzollovenecek@tuftsmedicalcenter.org>
2024-06-28 16:10:29 -0400

    Add new intermediate tables

--- tobacco.jl
+++ tobacco.jl
@@ -29,37 +29,54 @@ end
 TRDW.NotebookHeader("TRDW — Tobacco")
 
 # ╔═╡ 5fe23636-10fe-42e8-8814-3247211e3299
-## Accessing Tobacco Data in TRDW
+md"""## Accessing Tobacco Data in TRDW"""
 
 # ╔═╡ 64cae773-0478-4ec8-8a8c-c07a294fea92
-### Intermediate Model Tables
+md"""### Intermediate Model Tables"""
 
 # ╔═╡ 47385f71-c39d-4018-a4a3-fe4f88d9a525
-#### Composite (Epic and Legacy)
+md"""#### Composite (Epic and Legacy)"""
 
 # ╔═╡ 40a3df08-9841-465d-9d87-ba31cb58d2bd
-##### SOCIAL_HX_FULL
+md"""##### SOCIAL\_HX\_FULL"""
 
 # ╔═╡ d92682b0-4271-49c9-a25e-8398c004a109
-##### SDD_DATA_FULL
+md"""##### SDD\_DATA\_FULL"""
 
 # ╔═╡ afd86b45-7276-4b3f-9f90-c4929abb258e
-#### Epic-sourced
+md"""#### Epic-sourced"""
 
 # ╔═╡ d4ecf815-a8c3-48f9-bcf2-537c0c68e2cc
-##### SOCIAL_HX - Tobacco Subset
+md"""##### SOCIAL\_HX - Tobacco Subset"""
+
+# ╔═╡ 1457935b-7cfd-498b-9817-6d5629214d84
+@funsql shx_epic_tobacco() = from(shx_epic_tobacco)
 
 # ╔═╡ b602fc4d-7e08-41af-b591-a6169e444346
-##### SDD_DATA - Tobacco Subset
+md"""##### SDD\_DATA - Tobacco Subset"""
 
 # ╔═╡ 189ac0d0-acf1-4d9a-8140-2d2441c9b52b
-#### Legacy-sourced
+md"""#### Legacy-sourced"""
 
 # ╔═╡ 8f451a1a-ca46-4cc4-9de4-c07775bf9c21
-##### Soarian Diagnoses - Tobacco Subset
+md"""##### Soarian Diagnoses - Tobacco Subset
+
+This table is formatted in three ways: legacy, social_hx, and sdd"""
+
+# ╔═╡ 0df53a61-3f39-4d94-aad0-00150883e6f5
+begin
+	@funsql soarian_tobacco_dx() = from(soarian_tobacco_dx)
+	@funsql shx_soarian_tobacco_dx() = from(shx_soarian_tobacco_dx)
+end
 
 # ╔═╡ f4e7e7f7-c6c1-4118-87ab-9e5b8060fc34
-##### Soarian Observations - Tobacco Subset
+md"""##### Soarian Observations - Tobacco Subset"""
+
+# ╔═╡ 2bd2a35c-a347-4f0d-a3ce-defb56a8a4bb
+begin
+	@funsql soarian_tobacco_obs() = from(soarian_tobacco_obs)
+	@funsql shx_soarian_tobacco_obs() = from(shx_soarian_tobacco_obs)
+end
 
 # ╔═╡ d858cfa7-f0a0-4616-86da-9cebb90c6d65
 md"""## Appendix
@@ -76,17 +93,20 @@ TRDW.NotebookFooter()
 
 # ╔═╡ Cell order:
 # ╠═0db1f701-47e3-4755-b0d5-0b39a5fd1dbb
-# ╠═5fe23636-10fe-42e8-8814-3247211e3299
-# ╠═64cae773-0478-4ec8-8a8c-c07a294fea92
-# ╠═47385f71-c39d-4018-a4a3-fe4f88d9a525
-# ╠═40a3df08-9841-465d-9d87-ba31cb58d2bd
-# ╠═d92682b0-4271-49c9-a25e-8398c004a109
-# ╠═afd86b45-7276-4b3f-9f90-c4929abb258e
-# ╠═d4ecf815-a8c3-48f9-bcf2-537c0c68e2cc
-# ╠═b602fc4d-7e08-41af-b591-a6169e444346
-# ╠═189ac0d0-acf1-4d9a-8140-2d2441c9b52b
-# ╠═8f451a1a-ca46-4cc4-9de4-c07775bf9c21
-# ╠═f4e7e7f7-c6c1-4118-87ab-9e5b8060fc34
+# ╟─5fe23636-10fe-42e8-8814-3247211e3299
+# ╟─64cae773-0478-4ec8-8a8c-c07a294fea92
+# ╟─47385f71-c39d-4018-a4a3-fe4f88d9a525
+# ╟─40a3df08-9841-465d-9d87-ba31cb58d2bd
+# ╟─d92682b0-4271-49c9-a25e-8398c004a109
+# ╟─afd86b45-7276-4b3f-9f90-c4929abb258e
+# ╟─d4ecf815-a8c3-48f9-bcf2-537c0c68e2cc
+# ╠═1457935b-7cfd-498b-9817-6d5629214d84
+# ╟─b602fc4d-7e08-41af-b591-a6169e444346
+# ╟─189ac0d0-acf1-4d9a-8140-2d2441c9b52b
+# ╟─8f451a1a-ca46-4cc4-9de4-c07775bf9c21
+# ╠═0df53a61-3f39-4d94-aad0-00150883e6f5
+# ╟─f4e7e7f7-c6c1-4118-87ab-9e5b8060fc34
+# ╠═2bd2a35c-a347-4f0d-a3ce-defb56a8a4bb
 # ╟─d858cfa7-f0a0-4616-86da-9cebb90c6d65
 # ╠═c6f49bb2-783a-11ee-0151-47703d60127f
 # ╠═f082a987-c9b6-4330-812c-f1a7aa4cfb13
